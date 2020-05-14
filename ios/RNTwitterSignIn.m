c90033871b58c080faa762caee0268c34c6d4cdd
HackingGate <i@hackinggate.com>
2020-05-14 16:57:58 +0900

    Remove authNotResolved

--- ios/RNTwitterSignIn.m
+++ ios/RNTwitterSignIn.m
@@ -18,8 +18,6 @@
     return dispatch_get_main_queue();
 }
 
-BOOL authNotResolved = true;
-
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(init: (NSString *)consumerKey consumerSecret:(NSString *)consumerSecret resolver:(RCTPromiseResolveBlock)resolve
@@ -41,10 +39,7 @@ RCT_EXPORT_METHOD(logIn: (RCTPromiseResolveBlock)resolve
                                        @"userID":session.userID,
                                        @"email": requestedEmail,
                                        @"userName":session.userName};
-                if(authNotResolved){
-                    resolve(body);
-                    authNotResolved = false;
-                }
+                resolve(body);
             }];
         } else {
             reject(@"Error", @"Twitter signin error", error);
